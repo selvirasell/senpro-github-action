0bd77426267441c479136fcfa9adedadfaf8b427
selvirasell <aisaselviraquraataayunni@mail.ugm.ac.id>
2025-03-22 16:07:49 +0700

    update page index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
-/*
 import Head from 'next/head'
+/*
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
@@ -8,34 +8,34 @@ import styles from '@/styles/Home.module.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>Modul 01 - Lab 2.2 | Senior Project 2023</title>
-        <meta name="description" content="Template untuk Lab 2.2. Github Actions. Based on create next app" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Lakukan perubahan pada&nbsp;
-            <code className={styles.code}>src/pages/index.js</code>
-          </p>
-        </div>
+    return (
+        <>
+            <Head>
+                <title>Modul 01 - Lab 2.2 | Senior Project 2025</title>
+                <meta name="description" content="Template untuk Lab 2.2. Github Actions. Based on create next app" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <main>
+                <div>
+                    <p>
+                        Lakukan perubahan pada&nbsp;
+                        <code>src/pages/index.js</code>
+                    </p>
+                </div>
 
-        <div className={styles.center}>
-          <h1 className={inter.className}>
-            Senior Project 2023 | Lab Informatika
-          </h1>
-        </div>
+                <div>
+                    <h1>
+                        Senior Project 2023 | Lab Informatika
+                    </h1>
+                </div>
 
-        <div>
-          <h5 className={inter.className}>
-            [Aisa Selvira Quraata A'yunni] | [498561]
-          </h5>
-        </div>
-      </main>
-    </>
-  )
+                <div>
+                    <h5>
+                        [Aisa Selvira Quraata A'yunni] | [498561]
+                    </h5>
+                </div>
+            </main>
+        </>
+    )
 }
